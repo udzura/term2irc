2f8afcf772d336bf67f7b70c3a449d65acd35518
Uchio KONDO <udzura@pepabo.com>
2015-01-23 20:43:56 +0900

    Add a very simple testcase

--- test/lib/test_term2irc.rb
+++ test/lib/test_term2irc.rb
@@ -1,4 +1,8 @@
 require 'helper'
 
 class Term2IRCTest < Test::Unit::TestCase
+  test 'simple convert' do
+    result = Term2IRC.t2i("\e[1;4;30;43mgood morning\e[0m")
+    assert_equal "\u0002\u001F\u00031,8good morning\u000F", result
+  end
 end
